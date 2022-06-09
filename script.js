795098a7a66878dad59df10eb5f05d86b66e69a4
Erwan Soulas <erwan.soulas@gmail.com>
2022-06-09 10:58:07 +0200

    inititate game interface

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const btn2 = document.querySelector('#btn2');
 const btn3 = document.querySelector('#btn3');
 const btn4 = document.querySelector('#btn4');
 
+const sectionOne = document.querySelector('.sectionOne')
+const sectionTwo = document.querySelector('.sectionTwo')
+
 //Launch a music when the last button is clicked
 btn1.addEventListener('click', () => {
   malcomAudio.pause();
+  btn1Clicked();
 });
 
 btn2.addEventListener('click', () => {
   malcomAudio.pause();
+  btn2Clicked();
 });
 
 btn3.addEventListener('click', () => {
@@ -26,11 +31,28 @@ btn5.addEventListener('click', () => {
   malcomAudio.play();
 });
 
+function btn2Clicked () {
+  if (btn2.clicked = true) {
+    btn2.innerHTML = "Mauvaise réponse, tant pis pour toi";
+    btn2.style.background = "red";
+  }
+}
 
+function mouseOutbtn2() {
+  btn2.innerHTML = "NO";
+  btn2.style.background = "none";
+}
 
+//Inscription Interface
+function btn1Clicked() {
+  if (btn1.clicked = true) {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'block';
+  } 
+}
 
-
-
+const btnP1 = document.querySelector('#btnP1');
+const btnP2 = document.querySelector('#btnP2');
 
 
 
